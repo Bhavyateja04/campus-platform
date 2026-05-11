b9312ec426b74348ddcc009b705687ee42ea4bf7
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-05-11 15:08:16 +0530

    Updated collegeMemoriesRoutes.js

--- server/src/routes/collegeMemoriesRoutes.js
+++ server/src/routes/collegeMemoriesRoutes.js
@@ -1,11 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const { addMemory, getMemories, editMemory, deleteMemory  } = require('../controllers/collegeMemoriesController');
+const {
+  addMemory,
+  getMemories,
+  editMemory,
+  deleteMemory,
+} = require("../controllers/collegeMemoriesController");
 
-router.post('/add-memory', addMemory);
-router.get('/all-memories', getMemories);
-router.put('/edit-memory/:memoryId', editMemory);
-router.delete('/delete-memory/:memoryId', deleteMemory);
+// @route   POST    /api/memories
+router.post("/", addMemory);
+
+// @route   GET     /api/memories
+router.get("/", getMemories);
+
+// @route   PATCH   /api/memories/:memoryId
+router.patch("/:memoryId", editMemory);
+
+// @route   DELETE  /api/memories/:memoryId
+router.delete("/:memoryId", deleteMemory);
 
 module.exports = router;
