588d9370a6bd10698b9cd43a4bb36db992b119ae
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-05-11 15:06:40 +0530

    Update authRoutes.js

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const { login } = require("../controllers/authController");
 
+// @route   POST    /api/auth/login
 router.post("/login", login);
 
 module.exports = router;
