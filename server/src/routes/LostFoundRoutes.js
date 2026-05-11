ea35b5cb3f4d8e4c530a18c63363cf111a168695
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-05-11 15:04:11 +0530

    Updated LostFoundRoutes.js

--- server/src/routes/LostFoundRoutes.js
+++ server/src/routes/LostFoundRoutes.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const router = express.Router();
-const { createLostFoundItem, updateItem, viewItems, deleteItem } = require("../controllers/LostFoundController");
+
+const {
+  createLostFoundItem,
+  updateItem,
+  viewItems,
+  deleteItem,
+} = require("../controllers/LostFoundController");
 const protect = require("../middleware/authMiddleware");
-router.post("/createitem", protect, createLostFoundItem);
-router.get  ("/viewitems", protect, viewItems);
-router.put ("/updateitem/:id", protect,  updateItem);
-router.delete("/deleteitem/:id", protect, deleteItem);
+
+// @route   POST    /api/lostfound
+router.post("/", protect, createLostFoundItem);
+
+// @route   GET     /api/lostfound
+router.get("/", protect, viewItems);
+
+// @route   PUT     /api/lostfound/:id
+router.put("/:id", protect, updateItem);
+
+// @route   DELETE  /api/lostfound/:id
+router.delete("/:id", protect, deleteItem);
 
 module.exports = router;
