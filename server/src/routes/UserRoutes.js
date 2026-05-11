a68668989981f3ff536b1a628c63064b53a8f32f
Pothamsetti Satya Varshitha <23P31A1222@acet.ac.in>
2026-05-11 15:04:41 +0530

    Updated UserRoutes.js

--- server/src/routes/UserRoutes.js
+++ server/src/routes/UserRoutes.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const { updatePassword } = require("../controllers/UserController");
-const protect = require("../middleware/authMiddleware"); 
+const protect = require("../middleware/authMiddleware");
 
-router.put("/update-password", protect, updatePassword);
+// @route   PATCH   /api/users/password
+router.patch("/password", protect, updatePassword);
 
 module.exports = router;
